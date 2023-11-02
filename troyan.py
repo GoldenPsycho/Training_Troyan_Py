cd8d34564dbc48a1aaef69a1544d2b8e7de5e251
Maverick <goldenpsycho77@gmail.com>
2023-11-02 17:01:06 +0300

    'ff'

---
+++ troyan.py
@@ -0,0 +1,17 @@
+import sys
+
+from requests import get
+
+
+
+hostname = socket.gethostname()
+
+local_ip = socket.gethostbyname()
+
+puplic_ip = get('https://api.ipify.org').text
+
+print(f'Host: {hostname}')
+print(f'Local IP: {local_ip}')
+print(f'Public IP: {puplic_ip}')
+
+
